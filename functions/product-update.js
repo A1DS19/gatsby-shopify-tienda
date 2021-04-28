674d69a9e553397e560261627e045f277d587821
A1DS19 <jp06171@gmail.com>
2021-04-27 23:06:08 -0600

    netlify functions final

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,5 +1,6 @@
 const faunadb = require('faunadb');
 const verifyWebHook = require('shopify-verify-webhook');
+const axios = require('axios');
 
 const q = faunadb.query;
 
@@ -15,10 +16,49 @@ exports.handler = function (event, context, callback) {
   );
 
   if (isValid) {
-    callback(null, {
-      statusCode: 200,
-      body: 'Hello world',
+    const body = JSON.parse(event.body);
+    const { id } = body;
+
+    delete body.updated_at;
+    body.variants.forEach(v => {
+      delete v.updated_at;
+      delete v.inventory_quantity;
+      delete v.old_inventory_quantity;
     });
+
+    const bodyString = JSON.stringify(body);
+    client
+      .query(q.Get(q.Match(q.Index('product_by_id'), id)))
+      .then(result => {
+        if (result.data.product !== bodyString) {
+          client
+            .query(
+              q.Update(result.ref, {
+                data: { product: bodyString },
+              })
+            )
+            .then(() => {
+              axios.post(process.env.GATSBY_NETLIFY_BUILD_URL);
+            })
+            .catch(e => {
+              console.error(`ERROR FAUNA DB`, e);
+            });
+        }
+      })
+      .catch(err => {
+        client
+          .query(
+            q.Create(q.Collection('products'), {
+              data: { id, product: bodyString },
+            })
+          )
+          .then(() => {
+            axios.post(process.env.GATSBY_NETLIFY_BUILD_URL);
+          })
+          .catch(e => {
+            console.error(`ERROR FAUNA DB`, e);
+          });
+      });
   } else {
     callback(null, {
       statusCode: 403,
