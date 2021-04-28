a3e7c78e47f0939caea25209454d657fac91c985
A1DS19 <jp06171@gmail.com>
2021-04-27 22:53:20 -0600

    netlify functions 2

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,6 +1,28 @@
+const faunadb = require('faunadb');
+const verifyWebHook = require('shopify-verify-webhook');
+
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: process.env.GATSBY_FAUNA_DB_KEY,
+});
+
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello world',
-  });
+  const isValid = verifyWebHook(
+    process.env.GATSBY_SHOPIFY_WEBHOOK,
+    event.headers['x-shopify-hmac-sha256'],
+    event.body
+  );
+
+  if (isValid) {
+    callback(null, {
+      statusCode: 200,
+      body: 'Hello world',
+    });
+  } else {
+    callback(null, {
+      statusCode: 403,
+      body: 'Error',
+    });
+  }
 };
